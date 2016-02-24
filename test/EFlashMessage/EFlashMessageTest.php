34cb00c512cea6fac10dbd5c728088c46d3507ce
Elmseld <emily.elmseld@gmail.com>
2016-02-24 20:10:09 +0100

    Fixa testklass

--- test/EFlashMessage/EFlashMessageTest.php
+++ test/EFlashMessage/EFlashMessageTest.php
@@ -6,45 +6,143 @@ namespace Elms\EFlashMessage;
  * A testclass
  * 
  */
-class EFlashMessageTest extends \PHPUnit_Framework_TestCase
-{
+class EFlashMessageTest extends \PHPUnit_Framework_TestCase {
 
-	/**
-	 * Test
-	 *
-	 * @return void
-	 *
-	 */
-	public function testGetNameFromSession()
-	{
-			$msg = new \Elms\EFlashMessage\EFlashMessage();
-			$di    = new \Anax\DI\CDIFactoryDefault();
-			$msg->setDI($di);
-		
-		// Inject the comment service into the app
-			$di->setShared('msg', function() use ($di) {
-				$controller = new \Elms\EFlashMessage\EFlashMessage();
-				$controller->setDI($di);
-				return $controller;
-					});
-
-			$di->setShared('session', function () {
-					$session = new \Anax\Session\CSession();
-					$session->configure(ANAX_APP_PATH . 'config/session.php');
-					$session->name();
-					//$session->start();
-					return $session;
-			});
-
- 				$message = "testing message";
-        $msg->errorMessage($message);
-        $msg->warningMessage($message);
-        $msg->noticeMessage($message);
-        $msg->successMessage($message);
-        $messages = $msg->outputMsgs();
-        foreach ($messages as $index => $value) {
-            $this->assertEquals($message, $value['message'], "Form element value missmatch, method.");
-        }    
-		
-	}
-}
+     private $di;
+     private $message;
+     /**
+      * Setup
+      *
+      * @return void
+      */
+      public function setUp() {
+          $this->message = new \Elms\EFlashMessage\EFlashMessage();
+          $this->di = new \Anax\DI\CDIFactoryDefault();
+          $this->message->setDI($this->di);
+				
+          $this->di->setShared('session', function () {
+              $session = new \Anax\Session\CSession();
+              $session->configure(ANAX_APP_PATH . 'config/session.php');
+              $session->name();
+              //$session->start();
+              return $session;
+          });
+				
+          $this->di->setShared('flashmessage', function() {
+          	$flashMessages = new \Elms\EFlashMessage\EFlashMessage();
+          	$flashMessages->setDI($this->di);
+          	return $flashMessages;
+          });
+      }
+    /**
+     * Test
+     *
+     * @return void
+     */
+     public function testSetMessage() {
+        $type = 'Alert';
+        $text = 'Message text';
+        $this->message->flashmessage->setMessage('Alert', 'Message text');
+        $res = $this->di->session->get('flashmsgs');
+        foreach ($res as $key => $value) {
+            $this->assertEquals($type, $value['type'], "Type mismatch.");
+            $this->assertEquals($text, $value['content'], "Content mismatch.");
+        }
+     }
+     /**
+      * Test
+      *
+      * @return void
+      */
+      public function testAlert() {
+          $type = 'alert';
+          $text = 'Alert flash message';
+          $this->message->flashmessage->alert('Alert flash message');
+          $res = $this->di->session->get('flashmsgs');
+          foreach ($res as $key => $value) {
+              $this->assertEquals($type, $value['type'], "Type mismatch.");
+              $this->assertEquals($text, $value['content'], "Content mismatch.");
+          }
+      }
+      /**
+       * Test
+       *
+       * @return void
+       */
+       public function testError() {
+           $type = 'error';
+           $text = 'Error flash message';
+           $this->message->flashmessage->error('Error flash message');
+           $res = $this->di->session->get('flashmsgs');
+           foreach ($res as $key => $value) {
+               $this->assertEquals($type, $value['type'], "Type mismatch.");
+               $this->assertEquals($text, $value['content'], "Content mismatch.");
+           }
+       }
+       /**
+        * Test
+        *
+        * @return void
+        */
+        public function testInfo() {
+            $type = 'info';
+            $text = 'Info flash message';
+            $this->message->flashmessage->info('Info flash message');
+            $res = $this->di->session->get('flashmsgs');
+            foreach ($res as $key => $value) {
+                $this->assertEquals($type, $value['type'], "Type mismatch.");
+                $this->assertEquals($text, $value['content'], "Content mismatch.");
+            }
+        }
+        
+         /**
+          * Test
+          *
+          * @return void
+          */
+          public function testSuccess() {
+              $type = 'success';
+              $text = 'Success flash message';
+              $this->message->flashmessage->success('Success flash message');
+              $res = $this->di->session->get('flashmsgs');
+              foreach ($res as $key => $value) {
+                  $this->assertEquals($type, $value['type'], "Type mismatch.");
+                  $this->assertEquals($text, $value['content'], "Content mismatch.");
+              }
+          }
+           /**
+            * Test
+            *
+            * @return void
+            */
+            public function testOutputMsgs() {
+                $html = '<div class="alert"><p>Alert flash message</p></div>';
+                $html .= '<div class="error"><p>Error flash message</p></div>';
+                $html .= '<div class="info"><p>Info flash message</p></div>';
+                $html .= '<div class="notice"><p>Notice flash message</p></div>';
+                $html .= '<div class="warning"><p>Warning flash message</p></div>';
+                $html .= '<div class="success"><p>Success flash message</p></div>';
+                $this->message->flashmessage->alert('Alert flash message');
+                $this->message->flashmessage->error('Error flash message');
+                $this->message->flashmessage->info('Info flash message');
+                $this->message->flashmessage->success('Success flash message');
+                $htmloutput = $this->message->flashmessage->outputMsgs();
+                $this->assertEquals($html, $htmloutput, "Output does not match.");
+            }
+            /**
+             * Test
+             *
+             * @return void
+             */
+             public function testClearMessages() {
+                 $this->message->flashmessage->alert('Alert flash message');
+                 $this->message->flashmessage->error('Error flash message');
+                 $this->message->flashmessage->info('Info flash message');
+                 $this->message->flashmessage->success('Success flash message');
+                 // Check that we have messages in the array
+                 $this->assertTrue($this->di->session->has('flashmsgs'));
+                 $this->message->flashmessage->clearMessages();
+                 $res = $this->di->session->get('flashmsgs');
+                 $this->assertTrue(count($res) === 0);
+             }
+ }
