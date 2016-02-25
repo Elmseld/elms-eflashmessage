9d9183b72bb7f05a0638f55b9aecc5b4ca51a6c0
Elmseld <emily.elmseld@gmail.com>
2016-02-25 12:05:50 +0100

    fix i test

--- src/EFlashMessage/EFlashMessage.php
+++ src/EFlashMessage/EFlashMessage.php
@@ -9,106 +9,88 @@ namespace Elms\EFlashMessage;
  * Denna klass skapar en funktion för flash meddelande.
  */
 
-class EFlashMessage {
+    class EFlashMessage {
+			
+				/**
+				 * Initalerar kontrollen
+				 * 
+				 * @return void
+				 */
+        public function __construct() {
+    			if(!isset($_SESSION['msg'])) {
+           	$_SESSION['msg'] = array();
+        	}
+				}
+			
+					/**
+					 * Lägg till ett alert meddelande
+					 *
+					 * @param $message string med meddelandets test
+					 *
+					 * @return void
+					 */
+				public function alert($content) {
+					$message = array('content' => $content, 'type' => 'alert');
+					$_SESSION['msg'][] = $message;
+				}
 
-	
-	
-	/**
-	 * Initalerar kontrollen
-	 * 
-	 * @return void
-	 */
-    public function __construct() {
-        if (!isset($_SESSION['msg'])) {
-            $_SSESSION['msg'] = array();
-        }
-    }
 
-	/**
-	 * Lägg till medelande till session arrayen
-	 *
-	 * @param $type string med meddelandes typ
-	 * @param $content string med meddelandet
-	 *
-	 * @return void
-	 */
-	public function setMsg($type, $content) {
-		
-		$message = array('type' => $type, 'content' => $content);
-		$_SESSION['msg'][] = $message;
-	}
+					/**
+					 * Lägg till ett sucess meddelande
+					 *
+					 * @param $message string med meddelandets test
+					 *
+					 * @return void
+					 */
+				public function success($content) {
+					$message = array('content' => $content, 'type' => 'success');
+					$_SESSION['msg'][] = $message;
+				}
 
-	/**
-	 * Lägg till ett alert meddelande
-	 *
-	 * @param $message string med meddelandets test
-	 *
-	 * @return void
-	 */
-	public function alert($message) {
-		$alert = array('type' => 'alert', 'content' => $message);
-		$_SESSION['msg'][] = $alert;
-	}
-	
-		
-	/**
-	 * Lägg till ett sucess meddelande
-	 *
-	 * @param $message string med meddelandets test
-	 *
-	 * @return void
-	 */
-	public function success($message) {
-		$success = array('type' => 'success', 'content' => $message);
-		$_SESSION['msg'][] = $success;
-	}
-		
-	
-	/**
-	 * Lägg till ett error meddelande
-	 *
-	 * @param $message string med meddelandets test
-	 *
-	 * @return void
-	 */
-	public function error($message) {
-		$error = array('type' => 'error', 'content' => $message);
-		$_SESSION['msg'][] = $error;	}
-		
-	
-	/**
-	 * Lägg till ett informations meddelande
-	 *
-	 * @param $message string med meddelandets test
-	 *
-	 * @return void
-	 */
-	public function info($message) {
-		$info = array('type' => 'info', 'content' => $message);
-		$_SESSION['msg'][] = $info;
-	}
+					/**
+					 * Lägg till ett error meddelande
+					 *
+					 * @param $message string med meddelandets test
+					 *
+					 * @return void
+					 */
+				public function error($content) {
+					$message = array('content' => $content, 'type' => 'error');
+					$_SESSION['msg'][] = $message;
+				}
 
-	
-	/**
-	 * Prints the messages in the session flasher
-	 */
-	public function outputMsgs() {
-		$messages = $_SESSION['msg'];
-		$output = null;
+				/**
+				 * Lägg till ett informations meddelande
+				 *
+				 * @param $message string med meddelandets test
+				 *
+				 * @return void
+				 */
+				public function info($content) {
+					$message = array('content' => $content, 'type' => 'info');
+					$_SESSION['msg'][] = $message;
+				}
 
-		if(isset($messages)) {
-			foreach($messages as $key => $message) {
-				$output .= '<div class="' . $message['type'] . '"><p>' . $message['content'] . '</p></div>';
-			}
-		}
 
-			return $output;
-		}
+				/**
+	 			 * Prints the messages in the session flasher
+	 			 */
+				public function outputMsgs(){
+						$messages = $_SESSION['msg'];
+						$output = null;
+						if($messages) {
+								foreach ($messages as $key => $message) {
+										$output .= "<div class='" . $message['type'] . "'><p>" . $message['content'] . "</p></div>";
+								}
+						}
+						return $output;
 
-	/**
-	 * Clear messages in the session messenger
-	 */
-	public function clearMsg() {
-		$_SESSION['msg'] = null;
-	}
+				}
+
+				/**
+	 			 * Clear messages in the session messenger
+	       */
+				public function clearMsg() {
+					$_SESSION['msg'] = null;
+				} 
 }
--- test/EFlashMessage/EFlashMessageTest.php
+++ test/EFlashMessage/EFlashMessageTest.php
@@ -2,25 +2,44 @@
 
 namespace Elms\EFlashMessage;
 
-/**
- * A testclass
- * 
- */
-class EFlashMessageTest extends \PHPUnit_Framework_TestCase {
+class CFlashMessageTest extends \PHPUnit_Framework_TestCase {
 
 	public function testInfo() {
-	$test = new \Elms\EFlashMessage\EFlashMessage();
-					$test->info('content');
-					$res = $test->outputMsgs();
-					$exp = "<div class='type'><p>content</p></div>";
-					$this->assertEquals($res, $exp, "Missmatch");
+		$test = new \Elms\EFlashMessage\EFlashMessage();
+        $test->info('content');
+        $res = $test->outputMsgs();
+        $exp = "<div class='info'><p>content</p></div>";
+        $this->assertEquals($res, $exp, "Missmatch");
+	}
+	
+	public function testError() {
+		$test = new \Elms\EFlashMessage\EFlashMessage();
+        $test->error('content');
+        $res = $test->outputMsgs();
+        $exp = "<div class='error'><p>content</p></div>";
+        $this->assertEquals($res, $exp, "Missmatch");
+	}
+	
+	public function testSuccess() {
+		$test = new \Elms\EFlashMessage\EFlashMessage();
+        $test->success('content');
+        $res = $test->outputMsgs();
+        $exp = "<div class='success'><p>content</p></div>";
+        $this->assertEquals($res, $exp, "Missmatch");
+	}
+	
+	public function testAlert() {
+		$test = new \Elms\EFlashMessage\EFlashMessage();
+        $test->alert('content');
+        $res = $test->outputMsgs();
+        $exp = "<div class='alert'><p>content</p></div>";
+        $this->assertEquals($res, $exp, "Missmatch");
 	}
 	
 	public function testShowMsg() {
-
-	$test = new \Elms\EFlashMessage\EFlashMessage();
-					$res = $test->outputMsgs();
-					$exp = null;
-					$this->assertEquals($res, $exp, "Missmatch");
+		$test = new \Elms\EFlashMessage\EFlashMessage();
+				$res = $test->outputMsgs();
+				$exp = null;
+				$this->assertEquals($res, $exp, "Missmatch");
 	}
 }
